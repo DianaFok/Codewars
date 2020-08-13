920a9a4c1e474a5dbceeb64f9b788113b21bf743
Diana Fok <dianaisthebest@Dianas-MBP.fios-router.home>
2020-08-13 10:29:35 -0400

    8kyu codewar

---
+++ js/Grasshopper - Messi Goals.js
@@ -0,0 +1,14 @@
+// Use variables to find the sum of the goals Messi scored in 3 competitions
+// Information
+// Messi goal scoring statistics:
+//
+// Competition	Goals
+// La Liga	43
+// Champions League	10
+// Copa del Rey	5
+
+var laLigaGoals = 43
+var championsLeagueGoals =  10
+var copaDelReyGoals = 5
+
+var totalGoals = 58
