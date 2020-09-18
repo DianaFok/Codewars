2a9344ab1049d03d8f23ff27ff32493d94e68968
Diana Fok <diana.y.fok@gmail.com>
2020-09-18 11:50:29 -0400

    7kyu

---
+++ js/Simple equation reversal.js
@@ -0,0 +1,9 @@
+// Given a mathematical equation that has *,+,-,/, reverse it as follows:
+//
+// solve("100*b/y") = "y/b*100"
+// solve("a+b-c/d*30") = "30*d/c-b+a"
+
+function solve(eq){
+  return eq.split('').reverse().join('').replace(/(\d+)/gi,$1=>{
+  return $1.split('').reverse().join('')})
+}
