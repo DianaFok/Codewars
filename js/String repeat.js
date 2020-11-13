92604920475cfdb8cf41c6a3c4787187d63c2be8
Diana Fok <diana.y.fok@gmail.com>
2020-11-13 18:24:29 -0500

    8kyu

---
+++ js/String repeat.js
@@ -0,0 +1,8 @@
+// Write a function called repeat_str which repeats the given string src exactly count times.
+//
+// repeatStr(6, "I") // "IIIIII"
+// repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+
+function repeatStr (n, s) {
+  return s.repeat(n);
+}
