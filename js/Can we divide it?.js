186b939e9b7ee682529de50f9ba931d613d406f3
Diana Fok <diana.y.fok@gmail.com>
2021-03-30 15:06:37 -0400

    8kyu

--- js/Can we divide it?.js
+++ js/Can we divide it?.js
@@ -13,3 +13,74 @@
 // (15, -5, 3)   ->  true
 
 const isDivideBy = (number, a, b) => number % a === 0 && number % b === 0;
+
+
+// Given a valid (IPv4) IP address, return a defanged version of that IP address.
+//
+// A defanged IP address replaces every period "." with "[.]".
+//
+//
+//
+// Example 1:
+//
+// Input: address = "1.1.1.1"
+// Output: "1[.]1[.]1[.]1"
+// Example 2:
+//
+// Input: address = "255.100.50.0"
+// Output: "255[.]100[.]50[.]0"
+//
+//
+// Constraints:
+//
+// The given address is a valid IPv4 address.
+
+let defangIPaddr = function(address) {
+   return address.split('.').join('[.]')
+};
+
+
+// You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank. Return the wealth that the richest customer has.
+//
+// A customer's wealth is the amount of money they have in all their bank accounts. The richest customer is the customer that has the maximum wealth.
+//
+//
+//
+// Example 1:
+//
+// Input: accounts = [[1,2,3],[3,2,1]]
+// Output: 6
+// Explanation:
+// 1st customer has wealth = 1 + 2 + 3 = 6
+// 2nd customer has wealth = 3 + 2 + 1 = 6
+// Both customers are considered the richest with a wealth of 6 each, so return 6.
+// Example 2:
+//
+// Input: accounts = [[1,5],[7,3],[3,5]]
+// Output: 10
+// Explanation:
+// 1st customer has wealth = 6
+// 2nd customer has wealth = 10
+// 3rd customer has wealth = 8
+// The 2nd customer is the richest with a wealth of 10.
+// Example 3:
+//
+// Input: accounts = [[2,8,7],[7,1,3],[1,9,5]]
+// Output: 17
+//
+//
+// Constraints:
+//
+// m == accounts.length
+// n == accounts[i].length
+// 1 <= m, n <= 50
+// 1 <= accounts[i][j] <= 100
+
+const getSum = (acc, cur) => acc + cur;
+var maximumWealth = function(accounts) {
+    const sumArr = [];
+    for (let i = 0; i < accounts.length; i++){
+        sumArr.push(accounts[i].reduce(getSum, 0));
+    }
+    return Math.max(...sumArr)
+};
