81a80cde36535c8d9a19de56acaf6de204b8f172
Diana Fok <diana.y.fok@gmail.com>
2021-05-13 20:27:17 -0400

    8kyu - python

---
+++ js/Remove exclamation marks.js
@@ -0,0 +1,10 @@
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+// Hello World! = Hello World
+
+function removeExclamationMarks(s) {
+  return s.replace(/!/g, '');
+}
+
+//python
+def remove_exclamation_marks(s):
+    return s.replace('!', '')
